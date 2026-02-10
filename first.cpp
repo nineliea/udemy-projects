9ce01c4697d82d19c30e6ff9a781595b59f06886
unknown <nicelyn.23007@gmail.com>
2026-02-10 20:37:12 +0700

    Section 4

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> a; 
+
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << a << " is my favorite number!" << endl;
+
+    return 0;
+} 
